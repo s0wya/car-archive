3a52a168f90cd9229e4d6420ff57317066518d43
s0wya <patryk@conmir.pl>
2021-08-22 12:00:35 +0200

    init project

---
+++ src/main/java/pl/conmir/cararchive/CarArchiveApplication.java
@@ -0,0 +1,13 @@
+package pl.conmir.cararchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/conmir/cararchive/CarArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.conmir.cararchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
