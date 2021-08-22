88efad89edf7a84fa1119b5222574bc9cca187c1
s0wya <patryk@conmir.pl>
2021-08-22 13:28:11 +0200

    add model and tests

--- src/main/java/pl/conmir/cararchive/Car.java
+++ src/main/java/pl/conmir/cararchive/Car.java
@@ -2,17 +2,20 @@ package pl.conmir.cararchive;
 
 /*
     TODO:
-        -registration number
+        -registration number DONE
         -make
         -model
         -production year
  */
 
 
+import pl.conmir.cararchive.car.Model;
 import pl.conmir.cararchive.car.RegistrationNumber;
 
 public class Car {
 
     private RegistrationNumber registration;
+
+    private Model model;
 
 }
