dbdb54990fd5ad300f53d51e2c54b5b98704fe5d
s0wya <patryk@conmir.pl>
2021-08-22 12:42:50 +0200

    add car domain, registration number and tests

---
+++ src/main/java/pl/conmir/cararchive/Car.java
@@ -0,0 +1,18 @@
+package pl.conmir.cararchive;
+
+/*
+    TODO:
+        -registration number
+        -make
+        -model
+        -production year
+ */
+
+
+import pl.conmir.cararchive.car.RegistrationNumber;
+
+public class Car {
+
+    private RegistrationNumber registration;
+
+}
